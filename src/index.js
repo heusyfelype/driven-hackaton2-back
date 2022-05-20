4aa239c48e2020f34ff91e8d1f6cb5f4c392f4ae
heusyfelype <felype.heusy@gmail.com>
2022-05-20 17:29:46 -0300

    fix: corrigidos alguns bugs e construiída a estrutura básica com middlewares, controllers e routers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { mongoConnection } from './mongoDB.js';
+import { mongoConnection } from './mongoConnection.js';
 
 /*****************************************/
 
 // import route1 from "./routes/route1.js"
+// import route2 from "./routes/route2.js"
+//...
 
 /*****************************************/
-
 
 dotenv.config();
 
@@ -17,13 +18,16 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+
 //app.use(route1)
+//app.use(route2)
+//...
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 
     mongoConnection();
-    console.log('CoffeeShop API is Running!');
+    console.log('Hackaton2 API running!');
 
 });
